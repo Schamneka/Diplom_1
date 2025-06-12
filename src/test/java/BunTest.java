7b7068ee4130442e629a68b0ed7a89fe04df4828
Kirill Shamne <kirill.shamneka@yandex.ru>
2025-06-12 15:27:29 +0500

    BunTest

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,32 @@
+import praktikum.Bun;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class BunTest {
+    private Bun bun;
+
+    @Before
+    public void createNewInstance{
+        bun = new Bun("Краторная", 200.0f);
+
+    }
+
+    @Test
+    public void getName{
+        String expected = "Краторная";
+        String actual = bun.getName();
+
+        assertEquals("Неверное название булочки", expected,actual);
+    }
+
+    @Test
+    public void getPrice{
+        String expected = 200.0f;
+        String actual = bun.getPrice();
+
+        assertEquals("Неверная цена булочки", expected, actual, 0);
+    }
+}
